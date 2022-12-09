4d7fe94ea9fa881ecbd620c1cac401a30adcd9a1
Michael Eaton <mjeaton@github.com>
2022-12-08 20:51:06 -0500

    Code for the Dotnet Advent - Karvonen Heart Rate with C# and Spectre.Console

---
+++ 2022/karvonen/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+1. Create a program that prompts for your age and your resting heart rate. 
+2. Use the Karvonen formula to determine the target heart rate based on a range of
+intensities from 55% to 95%. 
+3. Generate a table with the results as shown in the example output.
+*/
+
+using Spectre.Console;
+
+Console.WriteLine();
+AnsiConsole.Markup("[green]Karvonen Heart Rate[/] ");
+Console.WriteLine();
+Console.WriteLine();
+
+// prompt for age 
+var age = AnsiConsole.Prompt(
+    new TextPrompt<int>("How old are you?")
+        .PromptStyle("green")
+        .ValidationErrorMessage("[red]That's not a valid age[/]")
+        .Validate(age =>
+            age switch
+            {
+                <= 16 => ValidationResult.Error("[red]You must at least be 16 years old.[/]"),
+                >= 101 => ValidationResult.Error("[red]You must be younger than 100.[/]"),
+                _ => ValidationResult.Success(),
+            })
+        );
+
+// prompt for resting heart rate
+var restingHeartRate = AnsiConsole.Prompt(
+    new TextPrompt<int>("What is your resting heart rate?")
+        .PromptStyle("green")
+        .ValidationErrorMessage("[red]That's not a valid heart rate[/]")
+        .Validate(rhr =>
+            rhr switch
+            {
+                <= 20 => ValidationResult.Error("[red]Your resting heart rate should be greater than 20.[/]"),
+                >= 101 => ValidationResult.Error("[red]Your resting heart rate should NOT be greater than 100.[/]"),
+                _ => ValidationResult.Success(),
+            })
+        );
+
+// TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+decimal lowerBounds = 55m;
+decimal upperBounds = 95m;
+int step = 5;
+
+var table = new Table();
+table.AddColumn("Intensity");
+table.AddColumn("Rate");
+
+for(decimal counter = lowerBounds; counter <= upperBounds; counter += step)
+{
+	var intensity = counter / 100;
+
+	var target = (int)(((220 - age - restingHeartRate) * intensity) + restingHeartRate);
+	table.AddRow($"{intensity:P0}", $"{target} bpm");
+}
+
+
+Console.WriteLine();
+Console.WriteLine($"Your maximum heart rate: {220 - age}");
+Console.WriteLine();
+
+AnsiConsole.Write(table);
---
+++ 2022/karvonen/README.md
@@ -0,0 +1,3 @@
+# Karvonen Heart Rate using C# and Spectre.Console
+
+The code that goes along with my [contribution](https://samestuffdifferentday.net/2022/12/10/karvonen-heart-rate/) to the [2022 Dotnet Advent](https://dotnet.christmas/).
